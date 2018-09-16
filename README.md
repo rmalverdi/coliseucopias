f7afd8a31666f6110bcad69382ff211df5b1b77c
Rodolfo Malverdi <rmalverdi@gmail.com>
2018-09-16 17:39:27 -0300

    Criando os arquivos principais para funcionamento do site: index.php, script.js, styles.css.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Site, futuro http://wwwcoliseucopias.com.br/ 
+
+ALteração do readme para descrever melhor o meu sistema.
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <link rel="shortcut icon" href="*" type="image/x-png"/>
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+		<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/script.js"></script>
+		<title>Coliseu Cópias</title>
+	</head>
+
+	<body>
+
+	</body>
+</html>
